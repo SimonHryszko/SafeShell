5741a10595fe9d0b81bdaf9353e75d6def1c2158
Simon Hryszko <s37198@s.pwste.edu.pl>
2023-08-20 03:57:41 +0200

    revert code

--- src/backup_sync.sh
+++ src/backup_sync.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-# Removed the entire function backup_sync()
+mode=$1
+work_tree=$(grep "work-tree" backup_sync_config | cut -d' ' -f2)
+git_dir=$(grep "git-dir" backup_sync_config | cut -d' ' -f2)
 
-backup_sync $1
-
+if [ "$mode" == "master" ]; then
+    git --work-tree=$work_tree --git-dir=$git_dir add -A
+    git --work-tree=$work_tree --git-dir=$git_dir commit -m "Generic commit message"
+    git --work-tree=$work_tree --git-dir=$git_dir pull
+    git --work-tree=$work_tree --git-dir=$git_dir push
+elif [ "$mode" == "slave" ]; then
+    git --work-tree=$work_tree --git-dir=$git_dir pull --force
+else
+    echo "Error: Invalid mode. Please specify 'master' or 'slave'."
+    exit 1
+fi
