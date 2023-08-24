21a326d3302868e84fde12c8fe9dc898539ddfff
Simon Hryszko <simon@hryszko.dev>
2023-08-24 18:12:51 +0200

    Auto backup

--- src/commit-ai.py
+++ src/commit-ai.py
@@ -7,26 +7,23 @@ from rich.prompt import Prompt
 
 console = Console()
 
+
 def load():
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# check if user in git repo
-def validate():
-    if not os.path.isdir(".git"):
-        print("Not a git repository")
-        sys.exit(1)
 
-# get git diff
 def get_diff():
     diff = os.popen("git diff --cached").read()
 
     if not diff:
-        console.log("No changes to commit")
+        console.log("No changes to commit or not in git repo.")
         sys.exit(1)
 
     return diff
 
 # get suggestions
+
+
 def get_suggestions(diff, model="gpt-4"):
     if model == "static":
         return "Update roles in i3_arch_setup and managed_by_bare_repo tasks"
@@ -35,8 +32,8 @@ def get_suggestions(diff, model="gpt-4"):
         model=model,
         messages=[
             {
-            "role": "system",
-            "content": "I want you to act as the author of a commit message in git. I'll enter a git diff, and your job is to convert it into a useful commit message. Do not preface the commit with anything, use the present tense, return the full sentence. Write only title, max 72 characters."
+                "role": "system",
+                "content": "I want you to act as the author of a commit message in git. I'll enter a git diff, and your job is to convert it into a useful commit message. Do not preface the commit with anything, use the present tense, return the full sentence. Write only title, max 72 characters."
             },
             {
                 "role": "user",
@@ -50,18 +47,22 @@ def get_suggestions(diff, model="gpt-4"):
         presence_penalty=0
     ).choices[0].message.content.capitalize()
 
+
 def commit(message):
     os.system(f"git commit -m '{message}'")
+
 
 def copy(message):
     os.system(f"echo '{message}' | xclip -sel clip")
 
-def generate_commit_message():
+
+def generate_commit_message(diff):
     console.log("Generating suggestions...")
-    message = get_suggestions(get_diff())
+    message = get_suggestions(diff)
     console.log(f"Suggestions: [bold]{message}[/bold]")
 
-    c = Prompt.ask("Options: [underline2]r[/underline2]etry, [underline2]c[/underline2]ommmit, cop[underline2]y[/underline2]", choices=['r', 'c', 'y'], default="c")
+    c = Prompt.ask("Options: [underline2]r[/underline2]etry, [underline2]c[/underline2]ommmit, cop[underline2]y[/underline2]",
+                   choices=['r', 'c', 'y'], default="c")
 
     if c == "r":
         return generate_commit_message()
@@ -70,7 +71,7 @@ def generate_commit_message():
     elif c == "y":
         return copy(message)
 
+
 if __name__ == "__main__":
     load()
-    validate()
-    generate_commit_message()
+    generate_commit_message(get_diff())
